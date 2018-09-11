ec514e4c00b5d2580a3aa66881bdfd322ef614f7
Howard Kitto <kitto.howard@gmail.com>
2018-09-11 21:11:46 +0100

    Output a 32 byte random number as hex

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,23 @@
 package main
 
 import "fmt"
+import "flag"
+import "crypto/rand"
+import "encoding/hex"
 
 func main() {
-	fmt.Println("Cli")
+	createMasterKeyPtr := flag.Bool("createMasterKey", false, "Create a new master key")
+
+	flag.Parse()
+
+	if *createMasterKeyPtr {
+		mk := make([]byte, 32)
+		rand.Read(mk)
+		mkStr := hex.EncodeToString(mk)
+
+		fmt.Println(string(mkStr))
+	} else {
+		fmt.Println("Not Gonna Make a Master Key")
+	}
+
 }
