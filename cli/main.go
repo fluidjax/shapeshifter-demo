55f4363bcc9e79ea6d786b34dd05dd0f9469729c
Howard Kitto <kitto.howard@gmail.com>
2018-09-12 09:52:32 +0100

    Imported BN254

--- cli/main.go
+++ cli/main.go
@@ -4,6 +4,14 @@ import "fmt"
 import "flag"
 import "crypto/rand"
 import "encoding/hex"
+import "amcl/BN254"
+
+func mpin254() {
+
+	var sha = BN254.HASH_TYPE
+
+	fmt.Print(sha)
+}
 
 func main() {
 	createMasterKeyPtr := flag.Bool("createMasterKey", false, "Create a new master key")
@@ -11,13 +19,14 @@ func main() {
 	flag.Parse()
 
 	if *createMasterKeyPtr {
+		mpin254()
+	} else {
+		fmt.Println("Gonna Make a Random Number")
 		mk := make([]byte, 32)
 		rand.Read(mk)
 		mkStr := hex.EncodeToString(mk)
 
 		fmt.Println(string(mkStr))
-	} else {
-		fmt.Println("Not Gonna Make a Master Key")
 	}
 
 }
