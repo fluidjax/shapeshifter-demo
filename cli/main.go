fe0c87c9acf90ef10e938e9165ee4258d962a250
Howard Kitto <kitto.howard@gmail.com>
2018-09-12 10:32:08 +0100

    Update README

--- cli/main.go
+++ cli/main.go
@@ -8,29 +8,28 @@ import "amcl"
 import "amcl/BN254"
 
 func printBinary(array []byte) {
-	for i:=0;i<len(array);i++ {
+	for i := 0; i < len(array); i++ {
 		fmt.Printf("%02x", array[i])
 	}
-	fmt.Printf("\n")
-} 
+}
 
 func mpin254(rng *amcl.RAND) {
 
-	var sha = BN254.HASH_TYPE
-	const MGS=BN254.MGS
+	const MGS = BN254.MGS
 	var S [MGS]byte
 
-	fmt.Println(sha)
 	/* Trusted Authority set-up */
 
 	fmt.Printf("\nTesting MPIN\n")
-	BN254.MPIN_RANDOM_GENERATE(rng,S[:])
-	fmt.Printf("Master Secret s: 0x");  printBinary(S[:])
+	BN254.MPIN_RANDOM_GENERATE(rng, S[:])
+	fmt.Printf("Master Secret s: 0x")
+	printBinary(S[:])
 }
 
 func main() {
-	createMasterKeyPtr := flag.Bool("createMasterKey", false, "Create a new master key")
 
+	//ToDo: Add some security to this
+	createMasterKeyPtr := flag.Bool("createMasterKey", false, "Create a new master key")
 	flag.Parse()
 
 	if *createMasterKeyPtr {
