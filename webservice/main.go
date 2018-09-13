9a7748caaede4a8b3217dabd987d57a75d131487
Howard Kitto <kitto.howard@gmail.com>
2018-09-13 12:56:43 +0100

    WS: Logging set up

--- webservice/main.go
+++ webservice/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	// "encoding/json"
-	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 // our main function
 func main() {
-	fmt.Println("server is running")
+
+	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	wrt := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(wrt)
+
+	log.Println("This is a test log entry")
+
+
+	
+
 	router := mux.NewRouter()
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
