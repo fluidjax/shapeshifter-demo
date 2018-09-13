04dbfc4436576c49830729c9d2b79876c0ad13ae
Howard Kitto <kitto.howard@gmail.com>
2018-09-13 13:22:50 +0100

    Timestamp route

--- webservice/main.go
+++ webservice/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-// our main function
+type status struct {
+	Timestamp time.Time `json:"timeStamp"`
+}
+
+func getStatus(w http.ResponseWriter, r *http.Request) {
+
+	var s status
+	s.Timestamp = time.Now()
+	log.Println("Request fulfilled at ", s.Timestamp)
+	json.NewEncoder(w).Encode(s)
+
+}
+
 func main() {
 
 	f, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
@@ -21,11 +35,7 @@ func main() {
 	wrt := io.MultiWriter(os.Stdout, f)
 	log.SetOutput(wrt)
 
-	log.Println("This is a test log entry")
-
-
-	
-
 	router := mux.NewRouter()
+	router.HandleFunc("/status", getStatus).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
