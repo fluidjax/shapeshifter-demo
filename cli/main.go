45d3e38124a80b1211bb99cc462ae3ee72126294
Howard Kitto <kitto.howard@gmail.com>
2018-09-13 07:08:51 +0100

    CLi working and most edge cases handled

--- cli/main.go
+++ cli/main.go
@@ -1,21 +1,23 @@
 package main
 
-import "fmt"
-import "flag"
-import "crypto/rand"
-import "encoding/hex"
-import "amcl"
-import "amcl/BN254"
-import "sync"
-import "encoding/json"
-import "io/ioutil"
-
-var lock sync.Mutex
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"amcl"
+	"amcl/BN254"
+	"bufio"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+)
 
 //Config is used to store persistent keys in /shapeshifter/config.json
 type Config struct {
-	MasterSecret [32]byte  `json:masterSecret`
-	ServerSecret [128]byte `json:serverSecret`
+	TimeStamp    time.Time `json:"timeStamp"`
+	MasterSecret string    `json:"mastersecret"`
+	ServerSecret string    `json:"serversecret"`
 }
 
 func printBinary(array []byte) {
@@ -25,67 +27,95 @@ func printBinary(array []byte) {
 	fmt.Printf("\n")
 }
 
-func mpin254(rng *amcl.RAND) {
-
+func genMasterKey() string {
 	const MGS = BN254.MGS
 	var S [MGS]byte
-
-	/* SST is needed to generate the server secret */
-	const MFS = BN254.MFS
-	const G2S = 4 * MFS /* Group 2 Size */
-	var SST [G2S]byte
-
-	/* Trusted Authority (Master Secret) set-up */
-	fmt.Printf("\nTesting MPIN\n")
+	rng := amcl.NewRAND()
+	var raw [100]byte
+	for i := 0; i < 100; i++ {
+		raw[i] = byte(i + 1)
+	}
+	rng.Seed(100, raw[:])
 	BN254.MPIN_RANDOM_GENERATE(rng, S[:])
-	// fmt.Printf("Master Secret s: 0x")
-	// printBinary(S[:])
+	SHex := hex.EncodeToString(S[:])
+	return SHex
 
-	var serverVals Config
-	serverVals.MasterSecret = S
+}
 
-	/* TODO: This will have its own cli flag */
+func genServerKey(S string) string {
 
-	BN254.MPIN_GET_SERVER_SECRET(S[:], SST[:])
-	// fmt.Printf("Server Secret SS: 0x")
-	// printBinary(SST[:])
+	var SSTHex string
 
-	serverVals.ServerSecret = SST
+	if S != ""{
+		const MFS = BN254.MFS
+		const G2S = 4 * MFS /* Group 2 Size */
+		var SST [G2S]byte
+		SByte, _ := hex.DecodeString(S)
+		BN254.MPIN_GET_SERVER_SECRET(SByte[:], SST[:])
+		SSTHex = hex.EncodeToString(SST[:])
+	}else{
+		fmt.Println("No Master Key Found")
+	}
+	return SSTHex
+}
 
+func writeConfigFile(serverVals Config) {
+	serverVals.TimeStamp = time.Now()
 	configJSON, _ := json.MarshalIndent(serverVals, "", "")
-	// fmt.Println(string(configJSON))
-
-	// writing json to file
-	// TODO: Need to write this somewhere more central
 	err := ioutil.WriteFile("../config.json", configJSON, 0644)
-
-	if err != nil{
-		fmt.Println(err)}
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
 
-	//ToDo: Add some security to this
+	//Read params from commandline. ToDo: Add some security to this
 	createMasterKeyPtr := flag.Bool("createMasterKey", false, "Create a new master key")
+	createServerKeyPtr := flag.Bool("createServerKey", false, "Create a new server key")
 	flag.Parse()
 
-	if *createMasterKeyPtr {
-		rng := amcl.NewRAND()
-		var raw [100]byte
-		for i := 0; i < 100; i++ {
-			raw[i] = byte(i + 1)
-		}
-		rng.Seed(100, raw[:])
-
-		mpin254(rng)
-
+	//Read serverConfig from Config file
+	var serverConfig Config
+	config, err := ioutil.ReadFile("../config.json")
+	if err != nil {
+		//ToDo: need to check for "no such file or directory"
+		fmt.Println(err)
 	} else {
-		fmt.Println("Gonna Make a Random Number")
-		mk := make([]byte, 32)
-		rand.Read(mk)
-		mkStr := hex.EncodeToString(mk)
-
-		fmt.Println(string(mkStr))
+		json.Unmarshal(config, &serverConfig)
 	}
 
+	if *createMasterKeyPtr {
+		if serverConfig.MasterSecret != ""{
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("You already have a Master Secret, are you sure? Press Y to continue")
+			decision, _ := reader.ReadString('\n')
+			if decision == "Y\n"{
+				serverConfig.MasterSecret = genMasterKey()
+				writeConfigFile(serverConfig)
+			}else{
+				fmt.Println("A Wise Choice")
+			}
+		}else{
+			serverConfig.MasterSecret = genMasterKey()
+			writeConfigFile(serverConfig)
+		}
+	}
+
+	if *createServerKeyPtr {
+		if serverConfig.ServerSecret != ""{
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("You already have a Server Secret, are you sure? Press Y to continue")
+			decision, _ := reader.ReadString('\n')
+			if decision == "Y\n"{
+				serverConfig.ServerSecret = genServerKey(serverConfig.MasterSecret)
+				writeConfigFile(serverConfig)
+			}else{
+				fmt.Println("A Wise Choice")
+			}
+		}else{
+			serverConfig.ServerSecret = genServerKey(serverConfig.MasterSecret)
+			writeConfigFile(serverConfig)
+		}
+	}
 }
